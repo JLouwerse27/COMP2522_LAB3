8dc16b048b25012cecd91b76369c418ea69c027b
JLouwerse27 <122394172+JLouwerse27@users.noreply.github.com>
2025-01-24 14:31:40 -0800

    First push

---
+++ src/code/IDevice.java
@@ -0,0 +1,68 @@
+
+
+
+
+abstract class IDevice
+{
+
+
+    private String purpose;
+
+    IDevice(final String purpose)
+    {
+        validatePurpose(purpose);
+        this.purpose = purpose;
+    }
+
+    private static void validatePurpose(final String purpose)
+    {
+        if(purpose == null || purpose.isBlank())
+        {
+            throw new IllegalArgumentException("null or blank purpose");
+        }
+    }
+
+    protected final String getPurpose()
+    {
+        return purpose;
+    }
+
+    abstract void printDetails();
+
+    @Override
+    public String toString()
+    {
+        return this.getClass().getSimpleName() + this.getPurpose();
+    }
+
+    @Override
+    public boolean equals(final Object obj)
+    {
+        if(obj instanceof IDevice)
+        {
+            return false;
+        }
+        else if (obj == this || obj.getClass().getSimpleName() == this.getClass().getSimpleName())
+        {
+            return true;
+        }
+        else
+        {
+         return false;
+        }
+
+    }
+
+
+    @Override
+    public int hashCode()
+    {
+        return
+    }
+
+
+
+
+
+
+}
